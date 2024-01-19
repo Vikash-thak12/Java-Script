5f050a885a3d3be364f0e7171d65f60cd753379a
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-19 20:52:47 +0530

    arrange the elements

--- 1st to 6th Day/6th_day.js
+++ 1st to 6th Day/6th_day.js
@@ -1,54 +1,54 @@
-// //Dom: Document object model in java Script
+//Dom: Document object model in java Script
 
-// //Window Object:
-// // The window object reprents an open window in a browser. It is a browser object & is automatically created by the browser 
-// // It's a global object with lots of properties and methods.
-
-
-// // What is dom?
-// // when a web page is loaded then the browser creates a document object model of the page 
-// // console.dir(window.document)  => used to print properties and methods of the document
+//Window Object:
+// The window object reprents an open window in a browser. It is a browser object & is automatically created by the browser 
+// It's a global object with lots of properties and methods.
 
 
+// What is dom?
+// when a web page is loaded then the browser creates a document object model of the page 
+// console.dir(window.document)  => used to print properties and methods of the document
 
 
-// //Dom manipulation 
-
-// //selecting with id
-
-// let heading = document.getElementById("heading");  //p
-// console.log(heading);
 
 
-// //selecting by classname => returns html collection     
-// let heading_class = document.getElementsByClassName("heading-class");
-// console.log(heading_class);
-// console.dir(heading_class);
+//Dom manipulation 
+
+//selecting with id
+
+let heading = document.getElementById("heading");  //p
+console.log(heading);
 
 
-// //selecting by tagname
+//selecting by classname => returns html collection     
+let heading_class = document.getElementsByClassName("heading-class");
+console.log(heading_class);
+console.dir(heading_class);
 
-// let parahs = document.getElementsByTagName("p");
-// console.log(parahs);
-// console.dir(parahs);
+
+//selecting by tagname
+
+let parahs = document.getElementsByTagName("p");
+console.log(parahs);
+console.dir(parahs);
 
 
-//query selector 
+// query selector 
 
-// let element = document.querySelector("p");
-// console.log(element);
-// console.dir(element);
+let element = document.querySelector("p");
+console.log(element);
+console.dir(element);
 
-// let allele = document.querySelectorAll("h1");
-// console.log(allele);
-// console.dir(allele);
+let allele = document.querySelectorAll("h1");
+console.log(allele);
+console.dir(allele);
 
-// let classes = document.querySelectorAll(".heading-class");
-// console.log(classes);
-// console.dir(classes);
+let classes = document.querySelectorAll(".heading-class");
+console.log(classes);
+console.dir(classes);
 
-// let myid = document.querySelector("#heading");
-// console.dir(myid);
+let myid = document.querySelector("#heading");
+console.dir(myid);
 
 
 
@@ -60,47 +60,47 @@
 // innerhtml = returns the plain text or html contents  in the element 
 // textcontent = returns textual content even for hidden elements
 
-// let ele = document.querySelector("p");
-// console.log(ele);
+let ele = document.querySelector("p");
+console.log(ele);
 
-// let div = document.querySelector("div");
-// console.log(div);
-// console.dir(div);
+let div_1 = document.querySelector("div");
+console.log(div_1);
+console.dir(div_1);
 
 
-//difference between innettext and innerhtml 
+// difference between innettext and innerhtml 
 // innerTEXT going to reutrn the actual plain text where as innerHTML going to return the plain text with its tags
 
-// let h1 = document.querySelector("h1");
-// console.dir(h1);
+let h1 = document.querySelector("h1");
+console.dir(h1);
 
 
 
 
-// //Questions:
+//Questions:
 // 1. Create a h2 heading element with text :  "hello JavaSCript". Append "From Apna College Students" to this text using JS.
 // 2. Create 3 divs with common class name - "box". Access them and add some unique text to each of them 
 
-// let h2 = document.querySelector("h2");
-// console.log(h2);
-// h2.innerText = h2.innerText + "from apna college students";
+let h2 = document.querySelector("h2");
+console.log(h2);
+h2.innerText = h2.innerText + "from apna college students";
 
 
 
-// let divs= document.querySelectorAll(".box");
-// console.dir(divs);
-// divs[0].innerText = "The land of lord Rama";
-// divs[1].innerText = "The land of lord Krishna";
-// divs[2].innerText = "The land of lord Shiva";
+let divs= document.querySelectorAll(".box");
+console.dir(divs);
+divs[0].innerText = "The land of lord Rama";
+divs[1].innerText = "The land of lord Krishna";
+divs[2].innerText = "The land of lord Shiva";
 
 
 // using loop to change the value of each div 
-// let idx = 1;
-// for(div of divs)
-// {
-//     div.innerText = `The value of the curent Div is: ${idx}`;
-//     idx++;
-// }
+let idx = 1;
+for(div of divs)
+{
+    div.innerText = `The value of the curent Div is: ${idx}`;
+    idx++;
+}
 
 
 
