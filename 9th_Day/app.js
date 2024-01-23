9deebb21a9d5c6448e6b0c3af1ddd8b94a85aa99
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-23 07:51:20 +0530

    Initial commit of callby hell

--- 9th_Day/app.js
+++ 9th_Day/app.js
@@ -1,37 +1,63 @@
-// Synchronous in JS
-// where the execution runs in a sequence manner that is one after another
+// // Synchronous in JS
+// // where the execution runs in a sequence manner that is one after another
 
-console.log("One");
-console.log("Two")
-console.log("Three");
-console.log("Four");
+// console.log("One");
+// console.log("Two")
+// console.log("Three");
+// console.log("Four");
 
-//Asynchoronous 
-// Asynchronous code execution allows to execute next instructions
-// immediately and doesn't block the flow
-
-
-console.log("Five");
-console.log("Six")
-
-setTimeout( () => {
-    console.log("Chal na a kaam karne de, Sarak wodhar:");
-}, 2000);
-
-console.log("Seven");
-console.log("Eight");
+// //Asynchoronous 
+// // Asynchronous code execution allows to execute next instructions
+// // immediately and doesn't block the flow
 
 
+// console.log("Five");
+// console.log("Six")
 
-// callback
-// function passed as an argument in another functoin is known as callback 
+// setTimeout( () => {
+//     console.log("Chal na a kaam karne de, Sarak wodhar:");
+// }, 2000);
 
-function sum (a,b) {
-    console.log(a+b);
+// console.log("Seven");
+// console.log("Eight");
+
+
+
+// // callback
+// // function passed as an argument in another functoin is known as callback 
+
+// function sum (a,b) {
+//     console.log(a+b);
+// };
+
+// function calculator (a,b,sumcallback) {  //here the sumcallback function is sum function which is called in another function so known as callback 
+//     sumcallback(a,b);
+// }
+
+// calculator(3,4,sum);
+
+
+//callback hell 
+
+function getData(DataId, NextData) {
+    setTimeout( () => {
+        console.log("Data:", DataId)
+        if(NextData) {
+            NextData();
+        }
+    }, 2000);
 };
 
-function calculator (a,b,sumcallback) {  //here the sumcallback function is sum function which is called in another function so known as callback 
-    sumcallback(a,b);
-}
-
-calculator(3,4,sum);
+getData(1, () => {
+    getData(2, () => {
+        getData(3, () => {
+            getData(4, () => {
+                getData(5, () => {
+                    getData(6, () => {
+                        getData(7);
+                    })
+                })
+            })
+        })
+    });
+});
