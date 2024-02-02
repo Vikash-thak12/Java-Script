0d3efe4e9f13971628deb49637c674cfbb8c5a6d
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-02 14:47:19 +0530

    Final commit

--- 10th_Day/app.js
+++ 10th_Day/app.js
@@ -2,13 +2,23 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 
 const getpara = document.querySelector(".para");
 const btn = document.querySelector("#btn");
+const test = document.querySelector(".test");
+
+
+  //innertext 
+  const res1 = document.querySelector("#real").innerText;
+  console.log(res1);
+
+  //textContent
+  const res2 = document.querySelector("#real").textContent;
+  console.log(res2);
 
 const getfacts = async () => {
     console.log("Getting Data....");
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
-    getpara.innerText = data[1].text;
+    getpara.innerText = data[1].text;   
 
 }
 
