c79be35187665091b41c9f2c6c7e03433ffac4a5
Vikash Thakur <thakvikash622@gmail.com>
2024-02-26 00:20:08 +0530

    Final commit on Map

--- Map/app.js
+++ Map/app.js
@@ -4,4 +4,23 @@ const newArr = arr.map( (item) => {
     return item + 10;
 })
 
-console.log(newArr);
+// console.log(newArr);
+
+const secondArr = arr.map( (item) => {
+    return item * 10;
+}).map((item) => {
+    return item + 2;
+})
+// In the above code there are two map first the map is going to return the value after mulfiply by  10 and then it will add 2
+
+console.log(secondArr);
+
+// Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+
+// console.log(fruits)
